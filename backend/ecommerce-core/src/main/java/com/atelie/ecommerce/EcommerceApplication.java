bd63e5df9a19908dd599762b2af758575cd81dc5
imd14s <eversond14s@outlook.com>
2026-01-24 21:35:14 -0300

    feat: iniciar backend spring boot com docker e testes base

---
+++ backend/ecommerce-core/src/main/java/com/atelie/ecommerce/EcommerceApplication.java
@@ -0,0 +1,17 @@
+package com.atelie.ecommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Classe principal da aplicação.
+ * Responsável apenas por iniciar o Spring Boot.
+ */
+@SpringBootApplication
+public class EcommerceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EcommerceApplication.class, args);
+    }
+
+}
---
+++ backend/ecommerce-core/src/test/java/com/atelie/ecommerce/EcommerceApplicationTests.java
@@ -0,0 +1,23 @@
+package com.atelie.ecommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+/**
+ * TESTE 01 (DDT/TDD) - Smoke Test
+ *
+ * Objetivo:
+ * - Garantir que o contexto do Spring Boot sobe sem erros.
+ *
+ * Critério de aprovação:
+ * - O teste passa se a aplicação inicializar no modo de testes.
+ */
+@SpringBootTest
+class EcommerceApplicationTests {
+
+    @Test
+    void contextLoads() {
+        // Se o Spring subir, o teste passa.
+    }
+
+}
